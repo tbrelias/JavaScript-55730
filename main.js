b4fa54e907a74f6ffda69f7e9f8b4ec85edd9280
Elias Tabare <tbrelias01@gmail.com>
2023-08-24 21:24:13 -0300

    Version 2.1

--- main.js
+++ main.js
@@ -1,47 +1,120 @@
-let nombreIngresado;
-let apellidoIngresado;
-let emailIngresado;
-let edadIngresada;
-let monto;
-let interesAnual;
-let cantidadAnios;
-let ganancias;
-let montoTotal;
+const Productos = [
+    {
+        id: '001',
+        marca: "Jonhnnie Walker",
+        nombre: "Whisky Blue Label",
+        region: "Escocia",
+        stock: 13,
+        precio: 58500  
+    },
 
-alert ("Bienvenidos a InverOne")
+    {
+        id: '002',
+        marca: "Jonhnnie Walker",
+        nombre: "Whisky Gold Label",
+        region: "Escocia",
+        stock: 22,
+        precio: 14000  
+    },
 
-nombreIngresado = prompt ("Ingrese su Nombre");
+    {
+        id: '003',
+        marca: "Jonhnnie Walker",
+        nombre: "Whisky Black Label",
+        region: "Escocia",
+        stock: 18,
+        precio: 10000  
+    },
 
-alert ("¡ Bienvenido al Centro de Inversiones InverOne" + " " + nombreIngresado + "!")
-console.log ("Su Nombre es:" + " " +nombreIngresado)
+    {
+        id: '004',
+        marca: "Jonhnnie Walker",
+        nombre: "Whisky Red Label",
+        region: "Escocia",
+        stock: 15,
+        precio: 5000 
+    },
+]
 
-apellidoIngresado = prompt ("Ingrese su Apellido")
-console.log ("Su Apellido es:" + " " +apellidoIngresado)
+let Carrito = []
 
-emailIngresado = prompt ("Ingrese su Email")
-console.log ("Su Email es:" + " " +emailIngresado)
+function DatosDelUsuario(){
 
-edadIngresada = parseInt(prompt("Ingrese su edad"))
-console.log (edadIngresada >18)
+    alert ("Bienvenidos a Bebradas")
 
-if (edadIngresada >=18) {
-    alert ("Usted es mayor de edad y responsable de cualquier accion a la que se comprometa")
+    let Nombre = prompt ("Ingrese su Nombre");
+    console.log ("Su Nombre es:" + " " +Nombre)
 
-    monto = parseInt(prompt("Ingrese monto de la inversion"))
+    let Apellido = prompt ("Ingrese su Apellido")
+    console.log ("Su Apellido es:" + " " +Apellido)
 
-    interesAnual = parseInt(prompt("Ingrese interes Anual de la Inversión"))
+    let DocumentoId = prompt ("Ingrese su numero de Documento")
+    console.log ("Su Numero de Documento es:" + " " +DocumentoId)
 
-    cantidadAnios = parseInt(prompt("Ingrese cantidad de años a invertir"))
-
-    montoTotal = monto - monto
-
-    for (let i = 0; i < cantidadAnios; i++) {
-    ganancias = monto * interesAnual / 100
-    montoTotal = monto + montoTotal + ganancias
-    console.log("Total: ", montoTotal)
-    }
-    alert ("Te esperamos en nuestras Sucursales para concretar ... Saludos")
-
-} else {
-    alert ("Usted es menor de Edad, por lo cual no podremos llegar a un acuerdo")
+    alert ("¡ Bienvenido a Bebradas" + " " + Nombre + "!")
 }
+
+DatosDelUsuario()
+
+function EleccionDeProductos(){
+    let Seleccion = prompt('¡ Hola! ¿ desea comprar algun producto ? Si/No')
+    while(Seleccion != "si" && Seleccion != "no"){
+        alert('Por favor ingresa si o no')
+        Seleccion = prompt('¡ Hola! ¿ desea comprar algun producto ? Si/No')
+    }
+
+    if (Seleccion == 'si'){
+        alert('A continuacion nuestra lista de pruductos')
+        let TodosLosProductos = Productos.map(
+            (Producto) => Producto.nombre + '' + Producto.precio + '$'
+        );
+        alert(TodosLosProductos.join(' / '))
+    } else if ( Seleccion == 'no'){
+        alert('¡Gracias por venir, Hasta pronto!')
+    }
+
+    while(Seleccion != 'no'){
+        let Producto = prompt('Agrega un producto a tu carrito')
+        let Precio = 0
+
+        if(Producto == 'Whisky Blue Label' || Producto == 'Whisky Gold Label' || Producto == 'Whisky Black Label' || Producto == 'Whisky Red Label' ){
+            switch (Producto) {
+                case 'Whisky Blue Label':
+                    Precio = 58500;
+                    break;
+                case 'Whisky Blue Label':
+                    Precio = 14000;
+                    break;
+                case 'Whisky Blue Label':
+                    Precio = 10000;
+                    break;
+                case 'Whisky Blue Label':
+                    Precio = 5000;
+                    break;
+                default:
+                    break;
+            }
+        let Unidades = parseInt(prompt('¿ Cuantas Unidades desea ?'))
+
+        Carrito.push({Producto, Unidades, Precio})
+        console.log(Carrito)
+        } else {
+            alert('No tenemos disponible ese producto')
+        }
+
+        Seleccion = prompt('¿Desea seguir comprando ?')
+
+        while(Seleccion === 'no'){
+            alert('¡ Gracias por la compra, Hasta pronto!')
+            Carrito.forEach((CarritoFinal) => {
+                console.log(`Producto: ${CarritoFinal.Producto}, Unidades: ${CarritoFinal.Unidades}, total a pagar por producto ${CarritoFinal.Unidades * CarritoFinal.Precio}`)
+            })
+        break; 
+        }
+    }
+}
+
+EleccionDeProductos()
+
+const Total = Carrito.reduce((acc, el) => acc + el.Precio * el.Unidades, 0)
+alert(`El total a pagar por su compra es: ${Total}`)
